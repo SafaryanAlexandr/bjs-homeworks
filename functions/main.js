879877588abd10a6ad9cdf3e22f831cbffed5b83
SafaryanAlexandr <11220099.as@gmail.com>
2019-07-11 01:12:56 +0300

    functions task 2,3

--- functions/main.js
+++ functions/main.js
@@ -36,3 +36,55 @@ function showSolutionMessage(a,b,c) {
 showSolutionMessage(1,2,3);
 showSolutionMessage(7,20,-3);
 showSolutionMessage(2,4,2);
+
+
+// задача №2
+function getAverageScore() {
+    let data = {
+        algebra: [2,4,5,2,3,4],
+        geometry: [2,4,5],
+        russian: [3,3,4,5],
+        music: [4,4,5]
+    };    
+    function calculationAverage (object) {
+        let totalMark = 0;
+        let numberMark = 0;
+        let result = {};
+        for (let subject in data) {
+            let sumMark = 0;
+            let averageMarkSubject;
+            for (let i=0; i < data[subject].length; i++) {
+                sumMark = sumMark + data[subject][i];//сумма оценок по предмету
+                numberMark++;//счетчик оценок общий
+            } 
+            totalMark = totalMark + sumMark;//сумма всех оценок
+            averageMarkSubject = sumMark / data[subject].length;//Средняя оценка по предмету
+            result[subject] = averageMarkSubject; 
+        }
+        let averageAll = (totalMark / numberMark).toFixed(1);//строка
+        result.average = averageAll;
+        console.log(result);
+    }
+    calculationAverage(data);
+ }
+ getAverageScore();
+
+ //задача №3
+ 
+function decipher(secretData){
+    let result = {};
+    if (secretData.aaa == 0) {
+      result.firstName = 'Родриго';
+    }
+    else if(secretData.aaa == 1) {
+      result.firstName = 'Эмильо';
+    }
+    if (secretData.bbb == 0) {
+      result.lastName = 'Родриго';
+    } 
+    else if (secretData.bbb == 1) {
+      result.lastName = 'Эмильо';
+    }
+    return result;
+ }
+console.log(decipher({aaa:1,bbb:1}));
