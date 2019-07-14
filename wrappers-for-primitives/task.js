c4fab6a1960de8ce8b517511fa9a78bc11ec5b8d
SafaryanAlexandr <11220099.as@gmail.com>
2019-07-14 15:22:53 +0300

    wa git status
    git commit -m wa

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,8 +1,12 @@
 function calculateMortgage() {
     let percent = window.percent.value;
+    console.log(percent);
     let contribution = window.contribution.value;
+    console.log(contribution);
     let amount = window.amount.value;
+    console.log(amount);
     let date = window.date.value;
+    console.log(date);
 
     let result = calculateTotalMortgage(percent, contribution, amount, date);
     let span = window.mortageResult;
@@ -10,6 +14,34 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    "use strict";//строгий режим
+    if(Number.isNaN(percent)){
+        console.log(`Задайте параметр 'Процентная ставка'`);
+        return;
+    }
+    if(Number.isNaN(contribution)){
+        console.log(`Задайте параметр 'Начальный взнос'`);
+        return; 
+    }
+    if(Number.isNaN(amount)){
+        console.log(`Задайте параметр 'Общая стоимость'`);
+        return; 
+    }
+    if (typeof percent !== 'number') {
+        console.log(`Параметр 'Процентная ставка' содержит неправильное значение ${percent}`);
+        return;
+    }
+    if (typeof contribution !== 'number') {
+        console.log(`Параметр 'Начальный взнос' содержит неправильное значение ${contribution}`);
+        return;
+    }
+    if (typeof amount !== 'number') {
+        console.log(`Параметр 'Общая стоимость' содержит неправильное значение ${amount}`);
+        return;
+    }
+    let amountBank = amount - contribution; // нужно веруть банку
+    let payMonth = amountBank
+     
 
     // код для задачи №1 писать здесь
     //return totalAmount;
@@ -23,6 +55,13 @@ function sayHello() {
 }
 
 function getGreeting(name) {
+    if(name.length === 0){
+        greeting =  'Привет, мир! Меня зовут Аноним.';
+    } else {
+        greeting =  `Привет, мир! Меня зовут ${name}.`;
+    }
+    return greeting;
+
     // код для задачи №2 писать здесь
     //return greeting;
 }
